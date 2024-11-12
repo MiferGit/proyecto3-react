2c37cc0e60c9d84c5ee7de403943876e50322c67
MiguelBelalcazar <mifer.code@gmail.com>
2024-11-12 16:13:21 -0500

    newProyect3

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,52 @@
-import { useEffect, useState } from 'react'
-import useFetch from './hooks/useFetch'
-import CardInfo from './assets/components/CardInfo'
-import ResidentsList from './assets/components/ResidentsList'
-import Search from './assets/components/Search'
-import './App.css'
+import { useEffect, useState } from "react";
+import useFetch from "./hooks/useFetch";
+import CardInfo from "./assets/components/CardInfo";
+import ResidentsList from "./assets/components/ResidentsList";
+import Search from "./assets/components/Search";
+import "./App.css";
 function App() {
-const [location, setLocation] = useFetch()
-const [locationId, setLocationId] = useState(1)
+  const [location, setLocation] = useFetch();
+  const [locationId, setLocationId] = useState(1);
+
+  //Page:
+  // const [page, setPage] = useState(1)
+  // const itemsPerPage = 9
+  // const totalItems = location? location.residents.length : 0
+  // const maxPage = Math.ceil(totalItems / itemsPerPage)
+  // console.log(maxPage)
 
 
+  useEffect(() => {
+    setLocation(`https://rickandmortyapi.com/api/location/${locationId}`);
+  }, [locationId]);
 
-useEffect(()=> {
-setLocation(`https://rickandmortyapi.com/api/location/${locationId}`)
-}, [locationId])
+  //Funtions Pages:
+//   const onPrev = ()=> {
+//     setPage(page - 1)
+//   }
 
+//   const onNext = ()=> {
+//     setPage(page + 1)
+//   }
 
-//**************************************************** */
+// const currentPageItems = location.residents.slice((page - 1) * itemsPerPage, page * itemsPerPage)
+  //**************************************************** */
   return (
     <>
-     {/* <div className='hero' /> */}
-     <div className='container'>
-      <div className='container__home'>
-        <div className='container__img'></div>
-        <div className='container__info'>
-         <Search setLocationId={setLocationId}/>
-         <CardInfo location={location}/>
+      <div className="container">
+        <div className="container__home">
+          <div className="container__img"></div>
+          <div className="container__info">
+            <Search setLocationId={setLocationId} />
+            <CardInfo location={location} />
+          </div>
         </div>
-     </div>
-     <ResidentsList residents={location?.residents}/>
-     </div>
-     </>
-  )
+        <ResidentsList residents={location?.residents} />
+          
+
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/assets/components/ResidentsList.jsx
+++ src/assets/components/ResidentsList.jsx
@@ -1,17 +1,56 @@
-import ResidentCard from "./ResidentCard"
-import './Styles/ResidentList.css'
+import { useState } from "react";
+import ResidentCard from "./ResidentCard";
+import "./Styles/ResidentList.css";
 
-function ResidentsList({residents}) {
+function ResidentsList({ residents }) {
+  const [page, setPage] = useState(1);
+  const itemsPerPage = 15;
+  const totalItems = residents ? residents.length : 0;
+  console.log(totalItems);
+  const maxPage = Math.ceil(totalItems / itemsPerPage);
+  console.log(maxPage);
+
+  // Funtions Pages:
+  const onPrev = () => {
+    setPage(page - 1);
+  };
+
+  const onNext = () => {
+    if (page < maxPage) {
+      setPage(page + 1);
+    }
+  };
+
+  const currentPageItems = residents
+    ? residents.slice((page - 1) * itemsPerPage, page * itemsPerPage)
+    : [];
+
   return (
-    <div className="cards">
-       {residents?.map(resident => {
-        const residentSplit = resident.split('/')
-        const id = residentSplit[residentSplit.length -1]
-        return(
-        <ResidentCard key={id} url={resident}/>)    
-})}
-    </div>
-  )
+    <>
+    
+      <div className="cards">
+        {currentPageItems.map((resident) => {
+          const residentSplit = resident.split("/");
+          const id = residentSplit[residentSplit.length - 1];
+          return <ResidentCard key={id} url={resident} />;
+        })}
+      </div>
+      
+      <div className="cards__btnPag">
+          <button className="cards__btnPag-pn" onClick={onPrev} disabled={page === 1}>
+            Anterior
+          </button>
+          <span className="cards__btn-span">
+            {" "}
+            {page}/{maxPage}📑{" "}
+          </span>
+          <button onClick={onNext} disabled={page === maxPage} className="cards__btnPag-pn">
+            siguiente
+          </button>
+        </div>
+       
+    </>
+  );
 }
 
-export default ResidentsList
+export default ResidentsList;
